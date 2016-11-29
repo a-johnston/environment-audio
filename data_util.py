3bd02374a3f779cf23b0a323cef4f223cc0ac9a2
Adam Johnston <amj69@case.edu>
2016-11-29 12:48:21 -0500

    fleshed out Dataset object to deal with training/testing split

--- data_util.py
+++ data_util.py
@@ -33,13 +33,60 @@ class WavData:
             data=self.data[math.ceil(start*self.fs):math.floor(end*self.fs)],
         )
 
+    def get_samples(self, sample_duration, include_tail=False):
+        """Returns a list of sample_duration second slices from this sample. If
+           sample_duration is None, [self] is returned.
+        """
+        if sample_duration is None:
+            return [self]
+
+        start = 0.0
+        segments = []
+
+        while start + sample_duration <= self.duration():
+            segments.append(self.slice(start, start + sample_duration))
+            start += sample_duration
+
+        if include_tail:
+            segments.append(self.slice(start, self.duration()))
+
+        return segments
+
     def fft(self):
         """Computes the FFT for the contained data
         """
         return fft(self.data)
 
 
-def load_labeled_data(data_folder='data'):
+class Dataset:
+    """Object for loading and 
+    """
+
+    def __init__(self, data_folder='data', split=0.9, sample_length=5.0):
+        """Loads the given dataset and performs a training/testing split using
+           the given percentage of total data.
+
+           Loaded WAV files are split into examples of duration sample_length
+           if provided. If sample_length is None, the WAV files are used as the
+           examples.
+        """
+        self.data = _load_labeled_data(data_folder, sample_length)
+
+        self.training = []
+        self.testing = []
+
+        for label in self.data:
+            y = self.data[label][0]
+            l = self.data[label][1]
+
+            split1 = l[:math.floor(len(l) * split)]
+            split2 = l[math.floor(len(l) * split):]
+
+            self.training += [(x, y) for x in split1]
+            self.testing += [(x, y) for x in split2]
+
+
+def _load_labeled_data(data_folder, sample_length):
     """Returns a list of labels and examples given a data_folder with the
        structure:
 
@@ -53,19 +100,20 @@ def load_labeled_data(data_folder='data'):
         List order may not be consistent between runs.
     """
     label_folders = os.listdir(data_folder)
-    m = map_label_to_one_hot(label_folders)
+    m = _map_label_to_one_hot(label_folders)
 
-    labeled_wavs = {}
+    labeled_data = {}
 
     for label in label_folders:
         files = os.listdir(os.path.join(data_folder, label))
         files = [os.path.join(data_folder, label, x) for x in files]
-        labeled_wavs[label] = (m[label], [WavData(f) for f in files])
+        samples = [WavData(f).get_samples(sample_length) for f in files]
+        labeled_data[label] = (m[label], sum(samples, []))
 
-    return labeled_wavs
+    return labeled_data
 
 
-def map_label_to_one_hot(labels):
+def _map_label_to_one_hot(labels):
     m = {}
 
     for i in range(len(labels)):
