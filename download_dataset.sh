81078779cb9a9a731863543125e5d82e5429678e
Adam J <amj69@case.edu>
2016-12-09 02:40:41 -0500

    added download script that cleans up and downloads new data

---
+++ download_dataset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# Requires gdrive to be set up. If you have access to this download, you will be
+# prompted for an access token.
+
+rm -rf data
+mkdir -p data
+gdrive download -r 0B7MwQ1GfoP1rZFpucGZVNXlZMkE
+mv 440data/* data/
