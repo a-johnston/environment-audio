9a989c4fc605253e03e4d592c4514efde7cb7e3a
Adam Johnston <amj69@case.edu>
2016-12-12 17:04:26 -0500

    updated plotter, included results for linear classifier

--- plotter.py
+++ plotter.py
@@ -8,7 +8,16 @@ if __name__ == '__main__':
     for f in sys.argv[1:]:
         with open(f) as data:
             points = json.load(data)
-            plt.plot(*zip(*points))
+            max_accuracy = max([p[1] for p in points])
+            base = 1472 / (1472 + 639)
+
+            x, y = zip(*points)
+
+            plt.plot(
+                x, y, 'b',
+                x, [max_accuracy] * len(x), 'g--',
+                x, [base] * len(x), 'r--',
+            )
             plt.xlabel('Iterations')
             plt.ylabel('Accuracy')
     plt.show()
