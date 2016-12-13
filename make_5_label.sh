e8596dc837369566db7427b0d7568582fee706ab
Adam Johnston <amj69@case.edu>
2016-12-13 13:07:18 -0500

    added 2-to-5 label mvs

---
+++ make_5_label.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+mkdir -p data/{l3,library,tink,thwing}
+mv data/inside/TinkCafeteriaIndoorsOpenSpace* data/tink/
+mv data/inside/Thwing* data/thwing/
+mv data/inside/L3* data/l3/
+mv data/inside/Library* data/library/
+rm -r data/inside
