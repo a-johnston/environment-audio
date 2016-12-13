577decbc639aefd8baf15008925da370f9f8ddf9
Adam Johnston <amj69@case.edu>
2016-12-12 21:53:08 -0500

    tons more runs, better plotter, some notes

--- plotter.py
+++ plotter.py
@@ -5,19 +5,32 @@ import sys
 
 
 if __name__ == '__main__':
+    args = []
+    base = 1472 / (1472 + 639)
+    x = []
+
+    colors = ['c', 'm', 'b']
+
     for f in sys.argv[1:]:
         with open(f) as data:
             points = json.load(data)
+            c = colors.pop()
             max_accuracy = max([p[1] for p in points])
-            base = 1472 / (1472 + 639)
 
             x, y = zip(*points)
 
-            plt.plot(
-                x, y, 'b',
-                x, [max_accuracy] * len(x), 'g--',
-                x, [base] * len(x), 'r--',
-            )
-            plt.xlabel('Iterations')
-            plt.ylabel('Accuracy')
+            args += [
+                x, y, c,
+                x, [max_accuracy] * len(x), c + '--',
+            ]
+
+    args += [
+        x, [base] * len(x), 'r--',
+    ]
+
+    plt.plot(*args)
+
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+
     plt.show()
